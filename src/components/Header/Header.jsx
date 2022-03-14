58e196ed3df2ae9190ab8d137610c9be0ff4e8bf
Ksenia <baranova-ksenia@mail.ru>
2022-03-14 16:08:30 +0200

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-
-import styles from './Header.module.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me.png'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Keren Oz</h1>
+        <h5 className='text-light'>Frontend Developer</h5>
+        <CTA />
+
+        <div className='photo'>
+          <img src={ME} alt='photo' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>
+          Scroll Down
+        </a>
+
+        <HeaderSocials />
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/Header/HeaderSocials.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { BsTelegram } from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='https://linkedin.com' target='__blank'>
+        <BsLinkedin />
+      </a>
+      <a href='https://github.com' target='__blank'>
+        <FaGithub />
+      </a>
+      <a href='https://telegram.com' target='__blank'>
+        <BsTelegram />
+      </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
